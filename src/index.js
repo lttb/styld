16da3030abbaab6b09800d4f0a76fc54b030ed21
lttb <connect@lttb.net>
2017-03-25 15:33:33 +0300

    Implement dynamic mods

--- src/index.js
+++ src/index.js
@@ -10,13 +10,44 @@ import prepareStyles, { styledClassFlag } from './prepare-styles'
 const JSS = createJSS(preset())
 
 
+const getClasses = ({ composes = [], classes = {} }) => composes
+  .reduce((acc, className) => acc.concat(classes[className] || []), [])
+  .join(' ')
+
+
 export const prepareStyled = ({ jss = JSS } = {}) => (styles) => {
   const stylesPrepared = prepareStyles(styles)
 
   const sheet = jss.createStyleSheet(stylesPrepared).attach()
   const Injector = injectSheet(stylesPrepared)
 
-  const createStyledElement = (tag, name = tag) => ({ children, attrs, ...data }) => {
+  const createStyledElement = (tag, name = tag) => ({ children, ...data }) => {
+    const { props, composes, attrs } = Object
+      .entries(data)
+      .reduce((acc, [key, value]) => {
+        if (key.startsWith(`$${styledClassFlag}`) && value) {
+          return Object.assign(acc, {
+            composes: acc.composes.add(name + key.slice(1)),
+          })
+        }
+
+        if (key.startsWith('$') && value) {
+          return Object.assign(acc, {
+            composes: acc.composes.add(key.slice(1)),
+          })
+        }
+
+        if (key.startsWith('_') && value) {
+          return Object.assign(acc, {
+            attrs: { ...acc.attrs, [key.slice(1)]: value },
+          })
+        }
+
+        return Object.assign(acc, {
+          props: { ...acc.props, [key]: value },
+        })
+      }, { props: {}, attrs: {}, composes: new Set([name]) })
+
     const Element = ({ classes }) => React.createElement(
       tag,
       {
@@ -26,13 +57,16 @@ export const prepareStyled = ({ jss = JSS } = {}) => (styles) => {
          * some problems with static/dynamic styles
          * @see https://github.com/cssinjs/react-jss/issues/75
          */
-        className: classes[name] || sheet.classes[name],
+        className: getClasses({
+          classes: { ...sheet.classes, ...classes },
+          composes: [...composes],
+        }),
       },
       children,
     )
 
-    if (Object.keys(data).length) {
-      return React.createElement(Injector(Element), data)
+    if (Object.keys(props).length) {
+      return React.createElement(Injector(Element), props)
     }
 
     return React.createElement(Element, sheet)
@@ -67,17 +101,24 @@ export const prepareStyled = ({ jss = JSS } = {}) => (styles) => {
 
 export const Styled = prepareStyled()
 
+const extend = (obj1, obj2 = {}) => Object
+  .entries(obj1)
+  .reduce((acc, [key, value]) => ({ ...acc, [key]: { ...value, ...obj2[key] } }), {})
+
 export default (styles, StyledFunc = Styled) => Element =>
   class StyledComponent extends Component {
-    styles = { ...styles, ...this.props.styles }
-
     componentWillMount() {
+      this.styles = this.setStyles(this.props.styles)
       this.styled = StyledFunc(this.styles)
     }
+
+    setStyles = (propStyles = {}) =>
+      ({ ...extend(styles, propStyles.extend), ...propStyles.assign })
 
     render = () => React.createElement(Element, {
       ...this.props,
       styled: this.styled,
+      $: this.styled,
       styles: this.styles,
     })
   }
